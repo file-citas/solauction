1512c16203ff5b014db60a925fd41c83295843b9
Felicitas Hetzelt <file@sect.tu-berlin.de>
2021-03-03 16:46:13 +0100

    add missing

---
+++ contracts/Miner.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.22 <0.9.0;
+// used to "waste" blocks for truffle tests
+contract Miner {
+    uint blocksMined;
+
+    constructor() public{
+        blocksMined = 0;
+    }
+
+    function mine() public {
+       blocksMined += 1;
+    }
+}
+
